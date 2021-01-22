60d6ab3081aea1a86332588b33a312c9b09c28a8
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2021-01-22 12:06:24 +0800

    Added Superslides and other contents

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $('#slides').superslides();
 })
+
+$(document).ready(function() {
+    $('#superslides').superslides();
+})
+
+$(document).ready(function() {
+    $('#slider').superslides();
+})
